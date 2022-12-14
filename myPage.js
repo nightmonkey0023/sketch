073213d6bdf120c15ce1e19ccbe34d345ec25165
nightmonkey0023 <120406385+nightmonkey0023@users.noreply.github.com>
2022-12-14 09:14:16 +0800

    Add files via upload

---
+++ myPage.js
@@ -0,0 +1,7 @@
+setTimeout(function() {
+		document.getElementById("loader").style.display="none";
+		document.getElementById("text").style.display="none";
+		},5000);
+		setTimeout(function() {
+			document.getElementById("form").style.display="block";
+		},6000);
